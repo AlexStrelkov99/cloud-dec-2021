225381916e77f9cac8b525eea03003c448f2f7b6
AlexStrelkov99 <58663166+AlexStrelkov99@users.noreply.github.com>
2022-10-24 10:58:32 +0300

    перемещение файла

--- src/main/java/com/geekbrains/chat/client/ChatController.java
+++ src/main/java/com/geekbrains/chat/client/ChatController.java
@@ -9,9 +9,15 @@ import javafx.scene.control.TextField;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class ChatController implements Initializable {
+    private Path clientDir;
     public TextField input;
     public ListView<String> listView;
     private IoNet net;
@@ -25,13 +31,21 @@ public class ChatController implements Initializable {
         Platform.runLater(() -> listView.getItems().add(msg));
 
     }
+    private void fillFileView() throws Exception{
+        List<String> files = Files.list(clientDir)
+                    .map(p -> p.getFileName().toString())
+                    .collect(Collectors.toList());
+        listView.getItems().addAll(files);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
+            clientDir = Paths.get("client");
+            fillFileView();
             Socket socket = new Socket("localhost", 8189);
             net = new IoNet(this::addMessage, socket);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
